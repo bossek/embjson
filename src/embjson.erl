f3e2971c4bd77312517ca26b3ff06505b0087143
bossek <lv.bossek@gmail.com>
2013-03-10 17:16:01 +0100

    Add license

--- src/embjson.erl
+++ src/embjson.erl
@@ -1,3 +1,27 @@
+%%%
+%%% Copyright 2013 Lubos Vesely
+%%%
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%
+%%% @author bossek <lv.bossek@gmail.com>
+%%% @doc Embedded JSON in Erlang sources
+%%%
+%%% Parse transform library allowing embedding JSON directly into Erlang
+%%% sources.
+%%%
+%%% @end
+%%%
+
 -module(embjson).
 
 -export([parse_transform/2]).
